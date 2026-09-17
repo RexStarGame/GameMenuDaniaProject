8f6bdcdb975d15dbd8b0f2c33d50b46e74e9feb9
FruJensen2013 <yamahafsi4@msn.com>
2026-09-17 14:09:07 +0200

    update

--- GameMenu/GameMenu/Program.cs
+++ GameMenu/GameMenu/Program.cs
@@ -16,14 +16,13 @@ Main();
         Console.Clear();
         Chess();
     }
-<<<<<<< Updated upstream
-=======
-    else if (menuSelect.Key==ConsoleKey.D3)
+
+    else if (menuSelect.Key==ConsoleKey.D4)
     {
         Console.Clear();
         PlayMineSweeper();
     }
->>>>>>> Stashed changes
+
 
         }
 
@@ -169,7 +168,7 @@ static void MainMastermind()
         }
 
         counter++;
-<<<<<<< Updated upstream
+
 
     }
     //if all 10 attempts have been used lost message will be given
@@ -178,7 +177,7 @@ static void MainMastermind()
         Console.WriteLine("Sorry you have lost \nPress ESC to GAME MENU \nPress ENTER to try again");
         ConsoleKeyInfo menuSelect = Console.ReadKey(true);
 
-=======
+
 
     }
     //if all 10 attempts have been used lost message will be given
@@ -609,7 +608,7 @@ const int bombCount = 40;
         Console.WriteLine("Press ENTER to retry \nPress ESC to GAME MENU");
         ConsoleKeyInfo menuSelect = Console.ReadKey(true);
 
->>>>>>> Stashed changes
+
         if (menuSelect.Key == ConsoleKey.Escape)
         {
             Console.Clear();
@@ -618,9 +617,9 @@ const int bombCount = 40;
         else if (menuSelect.Key == ConsoleKey.Enter)
         {
             Console.Clear();
-<<<<<<< Updated upstream
+
             MainMastermind();
-=======
+
             PlayMineSweeper();
         }
     }
@@ -782,241 +781,14 @@ const int bombCount = 40;
                 }
 
             }
->>>>>>> Stashed changes
+
         }
     }
 }
 
-<<<<<<< Updated upstream
-static void Chess()
-{
-    Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-    int[,] multiArray = new int[8, 8];
 
-    for (int col = 0; col < 8; col++)
-    {
-        //Grønne bønder
-        multiArray[1, col] = 1;
-    }
-    //Grønne tårne
-    multiArray[0, 0] = 2;
-    multiArray[0, 7] = 2;
-    //Grønne springere
-    multiArray[0, 1] = 3;
-    multiArray[0, 6] = 3;
-    //Grønne løbere
-    multiArray[0, 2] = 4;
-    multiArray[0, 5] = 4;
-    //Grøn konge
-    multiArray[0, 3] = 5;
-    //Grøn dronning
-    multiArray[0, 4] = 6;
 
-    for (int col = 0; col < 8; col++)
-    {
-        //Røde bønder
-        multiArray[6, col] = 7;
-    }
-    //Røde tårne
-    multiArray[7, 0] = 8;
-    multiArray[7, 7] = 8;
-    //Røde springere
-    multiArray[7, 6] = 9;
-    multiArray[7, 1] = 9;
-    //Røde løbere
-    multiArray[7, 5] = 10;
-    multiArray[7, 2] = 10;
-    //Rød konge
-    multiArray[7, 3] = 11;
-    //Rød dronning
-    multiArray[7, 4] = 12;
-
-    bool whiteTurn = true;
-    bool gameOver = false;
-
-    while (!gameOver)
-    {
-        Console.Clear();
-
-        Console.WriteLine("Chess\n");
-
-        if (whiteTurn)
-        {
-            Console.WriteLine("Green's turn\n");
-        }
-        else
-        {
-            Console.WriteLine("Red's turn\n");
-        }
-
-        //Chess board being created
-        for (int row = 0; row < multiArray.GetLength(0); row++)
-        {
-            Console.Write((8 - row) + " ");
-
-            for (int col = 0; col < multiArray.GetLength(1); col++)
-            {
-                if ((col + row) % 2 == 1)
-                {
-                    Console.BackgroundColor = ConsoleColor.Black;
-                }
-                else
-                {
-                    Console.BackgroundColor = ConsoleColor.White;
-                }
-
-                if (multiArray[row, col] >= 1 && multiArray[row, col] <= 6)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-                else if (multiArray[row, col] >= 7 && multiArray[row, col] <= 12)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-
-                //Green getting their pieces
-                if (multiArray[row, col] == 1)
-                {
-                    Console.Write("♙ ");
-                }
-                else if (multiArray[row, col] == 2)
-                {
-                    Console.Write("♖ ");
-                }
-                else if (multiArray[row, col] == 3)
-                {
-                    Console.Write("♘ ");
-                }
-                else if (multiArray[row, col] == 4)
-                {
-                    Console.Write("♗ ");
-                }
-                else if (multiArray[row, col] == 5)
-                {
-                    Console.Write("♕ ");
-                }
-                else if (multiArray[row, col] == 6)
-                {
-                    Console.Write("♔ ");
-                }
-                //Red getting their pieces
-                else if (multiArray[row, col] == 7)
-                {
-                    Console.Write("♙ ");
-                }
-                else if (multiArray[row, col] == 8)
-                {
-                    Console.Write("♖ ");
-                }
-                else if (multiArray[row, col] == 9)
-                {
-                    Console.Write("♘ ");
-                }
-                else if (multiArray[row, col] == 10)
-                {
-                    Console.Write("♗ ");
-                }
-                else if (multiArray[row, col] == 11)
-                {
-                    Console.Write("♕ ");
-                }
-                else if (multiArray[row, col] == 12)
-                {
-                    Console.Write("♔ ");
-                }
-                else
-                {
-                    Console.Write("  ");
-                }
-
-                Console.ResetColor();
-            }
-            Console.WriteLine();
-        }
-        Console.Write("  ");
-
-        for (char col = 'A'; col <= 'H'; col++)
-        {
-            Console.Write(col + " ");
-        }
-        Console.WriteLine();
-        Console.Write("\nFrom: ");
-        string from = Console.ReadLine() ?? "";
-
-        Console.Write("To: ");
-        string to = Console.ReadLine() ?? "";
-
-        char fromCol = from[0];
-        char fromRow = from[1];
-
-        char toCol = to[0];
-        char toRow = to[1];
-
-        int fromColNum = fromCol - 'A';
-        int toColNum = toCol - 'A';
-
-        int fromRowNum = 8 - (fromRow - '0');
-        int toRowNum = 8 - (toRow - '0');
-
-        int piece = multiArray[fromRowNum, fromColNum];
-        int targetPiece = multiArray[toRowNum, toColNum];
-
-        if (targetPiece == 11 && whiteTurn)
-        {
-            Console.WriteLine("\nGreen wins!\nPress ESC to GAME MENU \nPress ENTER to try again");
-            gameOver = true;
-            ConsoleKeyInfo menuSelect = Console.ReadKey(true);
-
-        if (menuSelect.Key == ConsoleKey.Escape)
-        {
-            Console.Clear();
-            Main();
-        }
-        else if (menuSelect.Key == ConsoleKey.Enter)
-        {
-            Console.Clear();
-            Chess();
-        }
-        }
-        else if (targetPiece == 5 && !whiteTurn)
-        {
-            Console.WriteLine("\nRed wins!\nPress ESC to GAME MENU \nPress ENTER to try again");
-            gameOver = true;
-            ConsoleKeyInfo menuSelect = Console.ReadKey(true);
-
-        if (menuSelect.Key == ConsoleKey.Escape)
-        {
-            Console.Clear();
-            Main();
-        }
-        else if (menuSelect.Key == ConsoleKey.Enter)
-        {
-            Console.Clear();
-            Chess();
-        }
-        }
-
-        if (gameOver)
-        {
-            break;
-        }
-
-        if (piece == 0)
-        {
-            Console.WriteLine("There is no piece on this position");
-            Console.ReadKey();
-            continue;
-        }
-
-        multiArray[toRowNum, toColNum] = piece;
-        multiArray[fromRowNum, fromColNum] = 0;
-        whiteTurn = !whiteTurn;
-
-        Console.WriteLine("You have moved from " + from + " to " + to);
-    }
-}
-=======
 enum MsValues
 {
     HiddenSafe = 0,
@@ -1027,7 +799,7 @@ enum MsValues
     Bomb = 5
 };
 
->>>>>>> Stashed changes
+
 
 
 
