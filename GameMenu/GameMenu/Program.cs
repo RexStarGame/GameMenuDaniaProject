ffa5e89a8a4ae2df5e002032a5ee991d429358c3
Laurids <lauridsjuhl@gmail.com>
2026-09-17 12:13:25 +0200

    Add nearby bombcount system

--- GameMenu/GameMenu/Program.cs
+++ GameMenu/GameMenu/Program.cs
@@ -17,40 +17,64 @@
             Safe = 1,
             FlagHiddenSafe = 2,
             HiddenBomb = 3,
-            FlagHiddenBomb = 4
+            FlagHiddenBomb = 4,
+            Bomb = 5
         };
         // MINESWEEPER BELLOW --------------------------------------------------------
         private static void PlayMineSweeper(string[] args)
         {
             const int bombCount = 40;
             string[] emojiList = {"⬛","⬜","🚩","💣"};
+            string[] numbList = { "0 ","1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 " };
             const int gridSize = 15;
             Random random = new Random();
             MsValues[,] grid = GenerateGameBoard();
             GenerateBombs(grid);
-            DrawGrid();
+            
             RequestInput();
 
             void RequestInput()
             {
+                DrawGrid();
                 Console.WriteLine("Choose a tile..");
                 
                 Console.Write("X = ");
                 string inputX = Console.ReadLine();
-                int xCoord = int.Parse(inputX);
+                int coordColumn = int.Parse(inputX);
                 
                 Console.Write("Y = ");
                 string inputY = Console.ReadLine();
-                int yCoord = int.Parse(inputY);
+                int coordRow = int.Parse(inputY);
                 
-                int[] coord = [xCoord, yCoord];
-                Console.WriteLine("What would you like to do at ("+xCoord+"."+yCoord+")?");
+                
+                int[] coord = [coordRow, coordColumn];
+                Console.WriteLine("What would you like to do at ("+coordColumn+"."+coordRow+")?");
                 Console.WriteLine(" ");
                 Console.WriteLine("   1. Reveal the tile.");
                 Console.WriteLine("   2. Flag/unflag the tile.");
                 Console.WriteLine("   3. Pick a different tile.");
                 string action = Console.ReadLine();
-                
+                switch (action)
+                {
+                    case "1": // reveal tile
+                        MsValues tileValue = RevealTile(coord);
+                        if (tileValue != MsValues.HiddenBomb)
+                        {
+                            RequestInput();
+                        }
+                        break;
+                    case "2": // Flag tile
+                        FlagTile(coord);
+                        RequestInput();
+                        break;
+                    case "3": // Pick different tile
+                        RequestInput();
+                        break;
+                    default: // invalid input
+                        Console.WriteLine(("INVALID INPUT"));
+                        RequestInput();
+                        break;
+                }
             }
 
             MsValues[,] GenerateGameBoard()
@@ -92,20 +116,23 @@
 
             }
 
-
-            /// <summary>
-            /// Do a check on a selected tile.
-            /// </summary>
-            void CheckTile(int[,] coord)
+            MsValues RevealTile(int[] coord)
             {
-                
+                MsValues value = GetTileValue(coord);
+                switch (value)
+                {
+                case MsValues.HiddenBomb:
+                    GameOver(coord);
+                    break;
+                case MsValues.HiddenSafe:
+                    SetTileValue(coord,MsValues.Safe);
+                    break;
+                }
+
+                return value;
             }
-            /// <summary>
-            /// Add, or remove, a flag from a hidden tile.
-            /// </summary>
-            /// <param name="x"></param>
-            /// <param name="y"></param>
-            /// <param name="add"></param> if true; add, else remove.
+            
+            
             void FlagTile(int[] coord)
             {
                 MsValues tileValue = GetTileValue(coord);
@@ -151,8 +178,11 @@
                 return GetTileValue(coord) == MsValues.FlagHiddenBomb || GetTileValue(coord) == MsValues.FlagHiddenSafe;
             }
 
-            void GameOver()
+            void GameOver(int[] coord)
             {
+                SetTileValue(coord,MsValues.Bomb);
+                DrawGrid();
+                Console.WriteLine("                             ____\n                     __,-~~/~    `---.\n                   _/_,---(      ,    )\n               __ /        <    /   )  \\___\n- ------===;;;'====------------------===;;;===----- -  -\n                  \\/  ~\"~\"~\"~\"~\"~\\~\"~)~\"/\n                  (_ (   \\  (     >    \\)\n                   \\_( _ <         >_>'\n                      ~ `-i' ::>|--\"\n                          I;|.|.|\n                         <|i::|i|`.\n                        (` ^'\"`-' \")\n");
                 Console.WriteLine("YOU LOSE! :(");
             }
 
@@ -181,13 +211,14 @@
                     {
                         string toWrite = "";
                         MsValues value = grid[x, y];
+                        int[] coord = [x, y];
                         switch(value)
                         {
                             case MsValues.HiddenSafe:
                                 toWrite = emojiList[0];
                                 break;
                             case MsValues.Safe:
-                                toWrite = emojiList[1];
+                                toWrite = GetSafeTileSprite(coord);
                                 break;
                             case MsValues.FlagHiddenSafe:
                                 toWrite = emojiList[2];
@@ -198,6 +229,9 @@
                             case MsValues.FlagHiddenBomb:
                                 toWrite = emojiList[2];
                                 break;
+                            case MsValues.Bomb:
+                                toWrite = emojiList[3];
+                                break;
                         }
                         Console.Write(toWrite+" ");
                     }
@@ -206,9 +240,55 @@
                 }
             }
 
-            string GetSafeTileSprite()
+            string GetSafeTileSprite(int[] coord)
             {
+                string output = emojiList[1];  //default revealed tile, with no bombs.
+                int bombCount = GetNearbyBombCount(coord);
+                bool nearbyBombs = bombCount > 0; 
+                if (nearbyBombs)
+                {
+                    output = numbList[bombCount];
+                }
                 
+                return output;
+            }
+
+            int GetNearbyBombCount(int[] coord)
+            {
+                int bombs = 0;
+                int row = coord[0];
+                int column = coord[1]; 
+
+                for (int rowOffset = -1; rowOffset <= 1; rowOffset++) // goes along the row
+                {
+                    for (int columnOffset = -1; columnOffset <= 1; columnOffset++) // goes along the column
+                    {
+                        if (rowOffset == 0 && columnOffset == 0) // this would be the coord tile, so skip.
+                        {
+                            continue; 
+                        }
+                        int nearbyRow = row + rowOffset;
+                        int nearbyColumn = column + columnOffset;
+                        
+                        if (nearbyRow < 0 || nearbyRow >= grid.GetLength(0) || nearbyColumn < 0 || nearbyColumn >= grid.GetLength(1))
+                        {
+                            continue; // skip if out of bounds
+                        }
+                        
+                        int[] nearbyCoord = {nearbyRow, nearbyColumn};
+                        if (isTileBomb(nearbyCoord))
+                        {
+                            bombs++;
+                        }
+                        
+                    }
+                }
+                return bombs;
+            }
+
+            bool isTileBomb(int[] coord)
+            {
+                return grid[coord[0],coord[1]] == MsValues.HiddenBomb || grid[coord[0],coord[1]] == MsValues.FlagHiddenBomb;
             }
         }
     }
